682fdfa5590451ebe49bd6e5a22883d987000587
cityhunter2468 <85825064+cityhunter2468@users.noreply.github.com>
2022-02-27 00:23:55 +0700

    ok

---
+++ src/java/entity/JavaMail.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright(C) 2005, G3-VS.
+ * Vehicle Store
+ *  
+ *
+ * Record of change:
+ * DATE            Version             AUTHOR           DESCRIPTION
+ * 2018-09-10      1.0                 MinhLH           First Implement
+ */
+package entity;
+
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.Session;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import org.hibernate.validator.constraints.Email;
+
+
+/**
+ *
+ * @author levan
+ */
+public class JavaMail {
+
+    public static void send(String to, String sub,
+            String msg, final String user, final String pass) {
+        //create an instance of Properties Class   
+        Properties props = new Properties();
+
+        /* Specifies the IP address of your default mail server
+     	   for e.g if you are using gmail server as an email sever
+           you will pass smtp.gmail.com as value of mail.smtp host. 
+           As shown here in the code. 
+           Change accordingly, if your email id is not a gmail id
+         */
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        //below mentioned mail.smtp.port is optional
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        /* Pass Properties object(props) and Authenticator object   
+           for authentication to Session instance 
+         */
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(user, pass);
+            }
+        });
+
+        try {
+
+            /* Create an instance of MimeMessage, 
+ 	      it accept MIME types and headers 
+             */
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(user));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject(sub);
+            message.setContent(msg, "text/html");
+
+            /* Transport class is used to deliver the message to the recipients */
+            Transport.send(message);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
