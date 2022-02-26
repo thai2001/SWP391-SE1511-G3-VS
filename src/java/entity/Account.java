e47fd79d032055ec0c9f1a56eccb367d40f01659
cityhunter2468 <85825064+cityhunter2468@users.noreply.github.com>
2022-02-27 00:20:33 +0700

    ok

--- src/java/entity/Account.java
+++ src/java/entity/Account.java
@@ -19,13 +19,43 @@ public class Account {
     private String username;
     private String password;
     private String status;
-    private Role roleId;    
+    private Role roleId;   
+    private int code;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private String description;
 
     public Account(String username, String password, String status, Role roleId) {
         this.username = username;
         this.password = password;
         this.status = status;
         this.roleId = roleId;
+    }
+
+    public Account(String username, String password, String status, Role roleId, int code, String name, String email, String phone) {
+        this.username = username;
+        this.password = password;
+        this.status = status;
+        this.roleId = roleId;
+        this.code = code;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Account(String username, String password, String status, Role roleId, int code, String name, String email, String phone, String address, String description) {
+        this.username = username;
+        this.password = password;
+        this.status = status;
+        this.roleId = roleId;
+        this.code = code;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.description = description;
     }
 
       
@@ -63,5 +93,55 @@ public class Account {
     public void setRoleId(Role roleId) {
         this.roleId = roleId;
     }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    
     
 }
