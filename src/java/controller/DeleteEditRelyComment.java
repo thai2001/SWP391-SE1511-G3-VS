dddf70425bb96cced8cf63f93d7bd281744262bc
cityhunter2468 <85825064+cityhunter2468@users.noreply.github.com>
2022-03-27 12:59:19 +0700

    oke

---
+++ src/java/controller/DeleteEditRelyComment.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright(C) 2005, G3-VS.
+ * Vehicle Store
+ *  
+ *
+ * Record of change:
+ * DATE            Version             AUTHOR           DESCRIPTION
+ * 2018-09-10      1.0                 MinhLH           First Implement
+ */
+package controller;
+
+import dao.CommentandRelyDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Admin
+ */
+public class DeleteEditRelyComment extends BaseReqAuth {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void processGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String id = request.getParameter("id");
+        String[] output = id.split("\\.");
+        CommentandRelyDAO cdb = new  CommentandRelyDAO();
+        if (output[0].equalsIgnoreCase("com"))
+        {
+            cdb.deleteComment(Integer.parseInt(output[1]));
+        }
+        
+        if (output[0].equalsIgnoreCase("rel"))
+        {
+            cdb.deleteRely(Integer.parseInt(output[1]));
+        }
+        response.getWriter().write(id);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void processPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String id = request.getParameter("id");
+        String content = request.getParameter("content");
+        String[] output = id.split("\\.");
+        CommentandRelyDAO cdb = new  CommentandRelyDAO();
+        cdb.EditComAndRel(output[0],Integer.parseInt(output[1]), content);
+        response.getWriter().write(id);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
